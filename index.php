e4030afd72ebbb419821bcbff53c3ab597982efc
Rait Kivinurm <rait.kivinurm@khk.ee>
2017-01-19 14:37:29 +0200

    Esimese faili loomine

--- index.php
+++ index.php
@@ -5,3 +5,18 @@
  * Date: 19.01.2017
  * Time: 12:47
  */
+
+class text
+{//text class begin
+    // class varibles = instsnce varibles
+
+    var $str = '';
+
+    //methods
+    //set text function
+
+    function setText($s){
+        $this->
+    }
+
+}
