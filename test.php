010b419efd206d3fc43098ede8ac6283cb9456d8
Rait Kivinurm <rait.kivinurm@khk.ee>
2017-01-30 14:08:20 +0200

    ctext red

--- test.php
+++ test.php
@@ -6,8 +6,9 @@
  * Time: 13:38
  */
 
-// require the object creating and using class
+// require the object creating and using classes
 require_once('text.php');
+require_once 'ctext.php';
 // create an object
 $sentence = new text();
 // control object output
@@ -15,7 +16,7 @@ echo '<pre>';
 print_r($sentence);
 echo '</pre>';
 // set text
-$sentence->setText('TERE text object!');
+$sentence->setText('Hello text object!');
 // control object output
 echo '<pre>';
 print_r($sentence);
@@ -24,20 +25,27 @@ echo '</pre>';
 $sentence->show();
 echo '<hr/>';
 // create an object
-$sentence2 = new text('Tere text by construct!');
-// control object output
-echo '<pre>';
-print_r($sentence);
-echo '</pre>';
-// show object output
-$sentence->show();
-echo '<hr/>';
-// create an object
-$sentence2 = new text('Hello text by construct!');
+$sentence2 = new text('TERE text by construct!');
 // control object output
 echo '<pre>';
 print_r($sentence2);
 echo '</pre>';
 // show object output
 $sentence2->show();
+echo '<hr/>';
+// create an object
+$sentence3 = new ctext('TERE color text by construct!');
+// control object output
+echo '<pre>';
+print_r($sentence3);
+echo '</pre>';
+// show object output
+$sentence3->show();
+// set object color
+$sentence3->setColor('#FF0000');
+echo '<pre>';
+print_r($sentence3);
+echo '</pre>';
+// show object output
+$sentence3->show();
 ?>
